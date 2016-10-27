a605ef77d0237aacf136f7dd163e4a162b72ef34
Jakub Jabłoński <jablonski.jak@gmail.com>
2016-10-27 14:50:00 +0200

    v2
    Polaczenie z baza

---
+++ src/Object/Zamowienie.java
@@ -0,0 +1,47 @@
+package Object;
+import java.sql.Date;
+
+public class Zamowienie {
+	private int idZamowienie;	
+	private Date dataZamowienia;
+	private String kontrahent;
+	
+	
+	public Zamowienie(Date dataZamowienia, String kontrahent) {
+		super();
+		this.dataZamowienia = dataZamowienia;
+		this.kontrahent = kontrahent;
+	}
+
+
+	public int getIdZamowienie() {
+		return idZamowienie;
+	}
+
+
+	public void setIdZamowienie(int idZamowienie) {
+		this.idZamowienie = idZamowienie;
+	}
+
+
+	public Date getDataZamowienia() {
+		return dataZamowienia;
+	}
+
+
+	public void setDataZamowienia(Date dataZamowienia) {
+		this.dataZamowienia = dataZamowienia;
+	}
+
+
+	public String getKontrahent() {
+		return kontrahent;
+	}
+
+
+	public void setKontrahent(String kontrahent) {
+		this.kontrahent = kontrahent;
+	}
+
+	
+}
--- src/Zamowienie.java
+++
@@ -1,8 +0,0 @@
-import java.sql.Date;
-
-public class Zamowienie {
-	private int idZamowienie;	
-	private Date dataZamowienia;
-	private String kontrahent;
-
-}
