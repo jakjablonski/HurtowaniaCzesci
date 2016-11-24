a4e1752c2ef75de76ba58be3614d62b461f22e53
Jakub Jabłoński <jablonski.jak@gmail.com>
2016-11-24 03:19:42 +0100

    mvn edit

--- src/test/java/com/ListaCzesciManagerTest.java
+++ src/test/java/com/ListaCzesciManagerTest.java
@@ -2,13 +2,21 @@ package com;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class ListaCzesciManagerTest {
+import com.manager.ListaCzesciManager;
 
+public class ListaCzesciManagerTest {
+	ListaCzesciManager listaczescimanager = new ListaCzesciManager();
+	
+	@Before
+	public void czysc_baze(){
+		listaczescimanager.UsunWszystkieListCzesc();
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_conection(){
+		assertNotNull(listaczescimanager.getConnection());
 	}
 
 }
--- src/test/java/com/ZamowienieManagerTest.java
+++ src/test/java/com/ZamowienieManagerTest.java
@@ -2,13 +2,22 @@ package com;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import com.manager.ZamowienieManager;
 
 public class ZamowienieManagerTest {
 
+	ZamowienieManager zamowienieManager = new ZamowienieManager();
+	
+	@Before
+	public void czysc_baze(){
+		zamowienieManager.UsunWszystkieZamowienie();
+	}
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_conection(){
+		assertNotNull(zamowienieManager.getConnection());
 	}
 
 }
